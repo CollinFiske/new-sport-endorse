dcce1954bbd21304866a7e5d7ee2c50897b784b8
Collin Fiske <132860072+CollinFiske@users.noreply.github.com>
2025-10-29 22:07:51 -0700

    now finished w/ new sub layout

--- components/BusinessSubscription.tsx
+++ components/BusinessSubscription.tsx
@@ -36,7 +36,7 @@ export default function BusinessSubscription() {
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     setFormData(prev => ({ ...prev, [name]: value }));
   };
